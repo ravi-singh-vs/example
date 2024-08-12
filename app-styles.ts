961e887099afae912cf39da9032eb047f6843861
RAVI SINGH <157360350+ravi-singh-vs@users.noreply.github.com>
2024-08-12 17:58:40 +0530

    Add files via upload

---
+++ app-styles.ts
@@ -0,0 +1,14 @@
+import { StyleSheet } from 'react-native'
+
+import { AppColors } from '@theme'
+
+const styles = StyleSheet.create({
+  appContainer: {
+    backgroundColor: AppColors.primary,
+  },
+  onBoardingContainer: {
+    backgroundColor: AppColors.secondary,
+  },
+})
+
+export default styles
